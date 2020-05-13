9fe0f69b4a4096e72bf849b6f50527dfcb67e40a
rockypiotr <rockypiotr72@gmail.com>
2020-05-13 07:32:21 +0200

    Now it looks better :o

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      dark
-      dense
-      collapse
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-app-toolbar-title>Hot16</v-app-toolbar-title>
+    <v-navigation-drawer v-model="drawer" app absolute temporary>
+      <v-list dense>
+        <v-list-item link @click="drawer = falsew">
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link @click="drawer = falsew">
+          <v-list-item-action>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Hot16Challenge2</v-toolbar-title>
     </v-app-bar>
-  <HelloWorld/>
+    <v-content>
+      <HelloWorld />
+    </v-content>
   </v-app>
 </template>
 
@@ -23,12 +41,20 @@ Vue.use(VueYoutube);
 export default {
   name: 'App',
 
+  props: {
+    source: String,
+  },
+
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+
   components: {
     HelloWorld,
   },
 
   data: () => ({
-    //
+    drawer: null,
   }),
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,40 @@
 <template>
   <v-container>
-    <ul id="example">
-      <li v-for="item in ytData.data.items" v-bind:key="item.id.videoId">
-        <h1>Tytuł: {{ item.snippet.title }}</h1>
-        <youtube :video-id="item.id.videoId" ref="youtube"/>
-        <p>Opis:</p>
-        <p>{{ item.snippet.description }}</p>
-      </li>
-    </ul>
+    <v-timeline>
+      <v-timeline-item v-for="item in ytData.data.items" v-bind:key="item.id.videoId" hide-dot>
+        <span slot="opposite">{{ item.snippet.publishTime }}</span>
+        <v-lazy
+          v-model="isActive"
+          :options="{
+            threshold: 0.5,
+          }"
+          min-height="500"
+          transition="fade-transition"
+        >
+          <v-card dark hover class="elevation-2">
+            <v-card-title class="title headline">Tytuł: {{ item.snippet.title }}</v-card-title>
+            <v-card-text class="text--primary pa-0">
+              <youtube class="center" :video-id="item.id.videoId" ref="youtube" fitParent />
+              <blockquote class="blockquote">{{ item.snippet.description }}</blockquote>
+            </v-card-text>
+          </v-card>
+        </v-lazy>
+      </v-timeline-item>
+    </v-timeline>
   </v-container>
 </template>
 
-<style lang="sass" scoped>
-  li
-    list-style-type: none
+<style lang="sass">
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed')
+
+li
+  list-style-type: none
+
+html, body
+  font-family: 'Roboto Condensed', sans-serif
+
+#app
+  font-family: 'Roboto Condensed', sans-serif
 </style>
 
 <script>
@@ -29,13 +50,18 @@ export default {
   data() {
     return {
       ytData: null,
+      hideDot: false,
     };
   },
 
   mounted() {
     axios
-      .get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&order=date&q=%23hot16challenge2&type=video&videoDefinition=high&key=AIzaSyCuBffhWIOOZlOg19GdJyz9x8YgCq-f2kE')
-      .then((response) => { this.ytData = response; });
+      .get(
+        'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&order=date&q=%23hot16challenge2&type=video&videoDefinition=high&key=AIzaSyCYdyp8ar2W8gOpDrIW1cRfeeVMqMcASH4',
+      )
+      .then((response) => {
+        this.ytData = response;
+      });
   },
 
   computed: {
